a0da6fe8ff9983d1a52dc965dee3e57fdbc7bae0
Eshwar0601 <852eshwar@gmail.com>
2021-01-12 18:00:05 +0530

    json-server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,6 +19,7 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from '../shared/baseUrl';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -138,7 +139,7 @@ function RenderDish({ dish }) {
   if (dish != null) {
     return (
       <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImg top src={baseUrl + '/' + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
